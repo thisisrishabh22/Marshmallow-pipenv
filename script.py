dde5fe70b5b8b620f0e7e0cf5d5e04ee10f2c6bc
Rishabh Singh <ris8645208@gmail.com>
2022-04-03 17:40:06 +0530

    feat: add basic & overidding validations

--- script.py
+++ script.py
@@ -1,35 +1,63 @@
-from marshmallow import Schema, fields, post_load
+from marshmallow import Schema, fields, post_load, ValidationError, validates
+
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, email, location):
         self.name = name
         self.age = age
-    
+        self.email = email
+        self.location = location
+
     def __repr__(self):
-        return f'{self.name} is {self.age} years old.'
+        return f'{self.name} is {self.age} years old has email {self.email} lives at {self.location}'
+
+# Using custom function
+# def validate_age(age):
+#     if age < 18 :
+#         raise ValidationError("The age is too young")
 
 
 class PersonSchema(Schema):
     name = fields.String()
+
+    # using custom fucntion
+    # age = fields.Integer(validate=validate_age)
+
+    # using decorator
     age = fields.Integer()
+    email = fields.Email()
+    location = fields.String(required=True)
+
+    # Using decorator
+    @validates("age")
+    def validate_age(self, age):
+        if age < 18:
+            raise ValidationError("The age is too young")
+        elif age > 150:
+            raise ValidationError("The age is too old")
 
     @post_load
-    def create_person(self, data,):
-        print(data)
+    def create_person(self, data, **kwargs):
         return Person(**data)
+
 
 input_data = {}
 
-input_data["name"] = input("What is your name? ")
-input_data["age"] = input("What is your age? ")
+try:
+    input_data["name"] = input("What is your name? ")
+    input_data["age"] = input("What is your age? ")
+    input_data["email"] = input("What is your email? ")
+    input_data["location"] = input("What is your location? ")
 
-schema = PersonSchema()
-person = schema.load(input_data)
+    schema = PersonSchema()
+    person = schema.load(input_data)
 
-# person = Person(name=input_data["name"], age=input_data["age"])
+    # person = Person(name=input_data["name"], age=input_data["age"])
+    print(person)
 
-print(person)
+    result = schema.dump(person)
 
-result = schema.dump(person)
-
-print(result)
+    print(result)
+except ValidationError as err:
+    print(err)
+    print(err.valid_data)
